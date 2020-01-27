77e17c9131fa8de0047282d1b6013d4ece519b70
artsyvak <asyvak@artelogic.net>
2020-01-27 03:01:11 +0200

    Updates

---
+++ precache-manifest.26cccd2e921dba998643764ee694f47c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7f9e4c91b02fc8df4c12916123f10e54",
+    "url": "/node-react-blog/index.html"
+  },
+  {
+    "revision": "43e8e35b0604f6cbcd79",
+    "url": "/node-react-blog/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "f405cb1dfc1539e426c4",
+    "url": "/node-react-blog/static/js/2.fae81f22.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/node-react-blog/static/js/2.fae81f22.chunk.js.LICENSE"
+  },
+  {
+    "revision": "43e8e35b0604f6cbcd79",
+    "url": "/node-react-blog/static/js/main.69da16fd.chunk.js"
+  },
+  {
+    "revision": "41c791eec77ac5884def",
+    "url": "/node-react-blog/static/js/runtime-main.95880d9c.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/node-react-blog/static/media/logo.5d5d9eef.svg"
+  }
+]);
